1625214911c67e86685c44df4326b9e55fb33be6
overkazaf <overkazaf@gmail.com>
2015-09-22 21:20:57 +0800

    add task excel

--- CombinationSumIII/main.js
+++ CombinationSumIII/main.js
@@ -10,30 +10,27 @@ var
 	globalN;
 
 var combinationSum3 = function(k, n) {
-    if (k === 0) return;
-    res = [];
-    temp = [];
-    globalK = k;
+    res     = [],
+    temp    = [],
+    globalK = k,
     globalN = 9;
+
     backtrack(k, n);
     return res;
 };
 
 var backtrack = function (k, n) {
-	if (k === 0) {
-		if (n === 0 && temp.length === globalK) {
-			res.push(temp.slice(0));	
-		}
+	if (k === 0 && n === 0 && temp.length === globalK) {
+		res.push(temp.slice(0));
 	} else {
 		for (var i = 1; i <= globalN; i++) {
 			if (temp.indexOf(i) === -1 && i <= n && ((!temp.length) || (i > temp[temp.length-1]))) {
 				temp.push(i);
-				console.log('i has been pushed:' + i);
-				backtrack(k-1, n - i);
+				backtrack( k - 1, n - i);
 				temp.length = temp.length-1;
 			}
 		}
 	}
 }
-
+console.log(5 & 6 & 7);
 console.log(combinationSum3(3, 9));
