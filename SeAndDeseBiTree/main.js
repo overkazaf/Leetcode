26534be3da08317e6c7dd39c608b9b0b2c44a666
John Mitnick <overkazaf@gmail.com>
2017-03-19 23:54:05 +0800

    add target solution

--- SeAndDeseBiTree/main.js
+++ SeAndDeseBiTree/main.js
@@ -1,17 +1,18 @@
- // Definition for a binary tree node.
- function TreeNode(val) {
-   this.val = val;
-   this.left = this.right = null;
- }
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
 
-
- /**
-  * Encodes a tree to a single string.
-  *
-  * @param {TreeNode} root
-  * @return {string}
-  */
- var serialize = function(root) {
+/**
+ * Encodes a tree to a single string.
+ *
+ * @param {TreeNode} root
+ * @return {string}
+ */
+var serialize = function(root) {
   var queue = [];
   var target = [];
   var p = root;
@@ -25,67 +26,57 @@
       if (current.left) {
         queue.push(current.left);
       }
-      target[c++] = current.left ? current.left.val : null;
+      target[c++] = current.left !== null ? current.left.val : null;
 
       if (current.right) {
         queue.push(current.right);
       }
 
-      target[c++] = current.right ? current.right.val : null;
+      target[c++] = current.right !== null ? current.right.val : null;
     }
-
+   
     while (target[--c] === null && c >= 0) {
       target.pop();
     }
   }
-
   return JSON.stringify(target);
  };
 
-
- /**
-  * Decodes your encoded data to tree.
-  *
-  * @param {string} data
-  * @return {TreeNode}
-  */
- var deserialize = function(data) {
+/**
+ * Decodes your encoded data to tree.
+ *
+ * @param {string} data
+ * @return {TreeNode}
+ */
+var deserialize = function(data) {
   var nodes = JSON.parse(data);
   if (nodes.length) {
-    var root = new TreeNode(nodes[0]);
-    dfs(root, nodes, 0);
-
+    var root = new TreeNode(nodes.shift());
+    var queue = [];
+    queue.push(root);
+    while (queue.length && nodes.length) {
+        var node = queue.shift();
+        var l = nodes.shift();
+        if (l !== null) {
+            node.left = new TreeNode(l);
+            queue.push(node.left);
+        }
+        if (!nodes.length) break;
+        var r = nodes.shift();
+        if (r !== null) {
+            node.right = new TreeNode(r);
+            queue.push(node.right);
+        }
+    }
+    
     return root;
   }
-
+  
   return null;
  };
+ 
 
- function dfs(node, arr, index) {
-  if (index > arr.length - 1) {
-    return;
-  }
-
-  var lIndex = 2 * index + 1;
-  var rIndex = 2 * index + 2;
-
-  if (lIndex < arr.length) {
-    node.left = new TreeNode(arr[lIndex]);
-    dfs(node.left, arr, lIndex);
-  }
-
-  if (rIndex < arr.length) {
-    node.right = new TreeNode(arr[rIndex]);
-    dfs(node.right, arr, rIndex);
-  }
- }
-
-  console.dir(serialize(deserialize("[ 1, 2, 3, null, null, 4, 5, 6, 7 ]")));
-  var c = deserialize("[ 1, 2, 3, null, null, 4, 5, 6, 7 ]");
-  console.dir(c);
-
-
- /**
-  * Your functions will be called as such:
-  * deserialize(serialize(root));
-  */
+/**
+ * Your functions will be called as such:
+ * deserialize(serialize(root));
+ */
